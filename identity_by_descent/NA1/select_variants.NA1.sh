c00d49e18fa20ff888f0c4fa314f31f2a4c8d685
Rory Craig <rory.craig@ed.ac.uk>
2018-12-23 12:04:45 +0000

    Delete select_variants.NA1.sh

--- identity_by_descent/NA1/select_variants.NA1.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-java -jar /opt/gatk/GenomeAnalysisTK.jar -T SelectVariants -R $PATH/chlamy.5.3.w_organelles_mtMinus.fasta -V $PATH/filter_snps.vcf --exclude_sample_file exclude_NA1.txt --removeUnusedAlternates -o NA1.snps.vcf 
