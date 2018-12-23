75b4ec626eeb541d86058627029a19f9b04ff3f2
Rory Craig <rory.craig@ed.ac.uk>
2018-12-23 12:04:21 +0000

    Delete select_variants.JPN.sh

--- identity_by_descent/JPN/select_variants.JPN.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-java -jar /opt/gatk/GenomeAnalysisTK.jar -T SelectVariants -R $PATH/chlamy.5.3.w_organelles_mtMinus.fasta -V $PATH/filter_snps.vcf --exclude_sample_file exclude_JPN.txt --removeUnusedAlternates -o JPN.snps.vcf 
