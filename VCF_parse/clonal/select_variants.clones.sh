eb668a9ba966e7abc7ab8b984cdc99298c981fc1
Rory Craig <rory.craig@ed.ac.uk>
2019-03-26 12:15:23 +0000

    Delete select_variants.clones.sh

--- VCF_parse/clonal/select_variants.clones.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for i in clone*txt ; do
-	root=`basename $i .txt`
-	java -jar /opt/gatk/GenomeAnalysisTK.jar -T SelectVariants -R /localdisk/data/chlamydomonas/popgen/references/chlamydomonas/5.3_chlamy_w_organelles_mt_minus/genome/chlamy.5.3.w_organelles_mtMinus.fasta -V /localdisk/home/s0920593/projects/biogeography/github/all_samples.vcf.gz --sample_file $i --removeUnusedAlternates -o $root.vcf
-done
